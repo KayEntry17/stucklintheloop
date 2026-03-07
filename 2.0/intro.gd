ea5c0da428eb23bbb32cfdcb7201330768a888d9
Kay <150719310+KayEntry17@users.noreply.github.com>
2026-03-07 01:58:04 +0000

    constant text speed, talk sounds + text skip

--- 2.0/intro.gd
+++ 2.0/intro.gd
@@ -1,6 +1,8 @@
 extends Node2D
 @export var nextlevel: PackedScene
+@export var themee:AudioStream
 func _ready() -> void:
+	Mus.swaptrack(themee)
 	$"Cat/AnimationPlayer".play("begin")
 func end():
 	TR.change_scene(nextlevel)
--- ui/nine_patch_rect.gd
+++ ui/nine_patch_rect.gd
@@ -7,14 +7,29 @@ extends NinePatchRect
 @export var moreboxes: bool
 @export var pointpos:Vector2
 @export var newpose: Vector2
-
+var vischr:float
 var posttoskip: bool
 @export var textspeed:float=2
 func _process(delta: float) -> void:
+	
+	if $Label.visible_characters>0 and $Label.visible_ratio<1:
+		if $AudioStreamPlayer.playing==false:
+			$AudioStreamPlayer.play()
+		vischr+=delta*textspeed*50
+		$Label.visible_characters=int(vischr)
+	else:
+		$AudioStreamPlayer.stop()
 	if !Engine.is_editor_hint():
-		if posttoskip:
-			if Input.is_action_just_pressed("ui_accept"):
+		if Input.is_action_just_pressed("ui_accept"):
+			if posttoskip:
+				
+			
 				kill_self()
+			else:
+				$Label.visible_ratio=1
+				posttoskip=true
+		if $Label.visible_ratio==1:
+			posttoskip=true
 	else:
 		if newpose!=Vector2(0,0):
 			pointpos=newpose
@@ -23,8 +38,9 @@ func _process(delta: float) -> void:
 		get_child(0).size=Vector2(fullsize.x-6, fullsize.y-8)
 		get_child(0).position=Vector2(3,3)
 	#get_child(0).text=text
+		
 func _ready() -> void:
-	
+	self.scale=Vector2(1,1)
 
 	global_position=pointpos-Vector2(0,size.y)
 	#print(global_position)
@@ -32,14 +48,15 @@ func _ready() -> void:
 	get_child(0).size=Vector2(fullsize.x-6, fullsize.y-8)
 	get_child(0).position=Vector2(3,3)
 	posttoskip=false
-	$active.wait_time=textspeed
-	$active.start()
+	#$active.wait_time=$Label.get_total_character_count()/textspeed/50
+	#$active.start()
 	Pausem.pause()
 	$ColorRect.mouse_filter=MOUSE_FILTER_STOP
-	var tween = get_tree().create_tween().set_trans(Tween.TRANS_LINEAR)
-	tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
-	$Label.visible_ratio=0
-	tween.tween_property($Label, "visible_ratio", 1, textspeed)
+	#var tween = get_tree().create_tween().set_trans(Tween.TRANS_LINEAR)
+	#tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	$Label.visible_characters=1
+	vischr=1
+	#tween.tween_property($Label, "visible_ratio", 1, textspeed)
 func next_slide():
 	if moreboxes:
 		var nb=nextbox.instantiate()
@@ -48,11 +65,12 @@ func next_slide():
 		get_parent().add_child(nb)
 		
 func kill_self():
+	visible=false
 	Pausem.unpause()
 	next_slide()
 	$ColorRect.mouse_filter=MOUSE_FILTER_IGNORE
 	var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE)
-	tween.tween_property(self, "global_position", Vector2(1000,1000), 0.2)
+	tween.tween_property(self, "scale", Vector2(0,0), 0.2)
 	$death13.start()
 	posttoskip=false
 	
@@ -61,8 +79,8 @@ func kill_self():
 	
 
 
-func _on_active_timeout() -> void:
-	posttoskip=true
+#func _on_active_timeout() -> void:
+	#posttoskip=true
 
 
 func _on_death_13_timeout() -> void:
